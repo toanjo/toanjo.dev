b60ce41a46582fc23d1940d4265b76dbe79f42aa
António Anjo <to.anjo57@gmail.com>
2019-12-13 23:46:40 +0000

    almost done, missing code review and content

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,7 @@ import React from "react"
 import footerStyles from './footer.module.scss'
 
 const Footer = () => <footer className={footerStyles.footer}>
-    <small className={footerStyles.footerText}>Built with <a className={footerStyles.footerLinks} href="https://gatsbyjs.org" target="_blank" rel="noopener noreferrer">Gatsby</a>.</small>
+    <small className={footerStyles.footerText}>Built by Tó with <a className={footerStyles.footerLinks} href="https://gatsbyjs.org" target="_blank" rel="noopener noreferrer">Gatsby</a>, © 2019</small>
 </footer>
 
 export default Footer
